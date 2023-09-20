3a42af8409e924daaaa9b11aa46ab423c4bbb65b
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-21 00:21:32 +0200

    week1

---
+++ week1/ConvertCurrency.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class ConvertCurrency {
+
+    private static final float CONVERSION_RATE = 99.91f;
+
+    public static void main(String[] args) {
+
+        int eurValue = 10;
+        float npr = eurValue * CONVERSION_RATE;
+
+        System.out.println(eurValue + " EUR equals to " + npr + " NPR");
+    }
+}
