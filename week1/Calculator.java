7db91370297e6e8b0f75d8cfcad16e205024d844
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-21 00:03:04 +0200

    week1

---
+++ week1/Calculator.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // implementation of calculator logic
+        int num1 = 10;
+        int num2 = 3;
+
+        int add = num1 + num2;
+        int subtract = num1 - num2;
+        int multiply = num1 * num2;
+        int divide = num1 / num2;
+        int modulo = num1 % num2;
+
+        System.out.println(num1 + " + " + num2 + " = " + add);
+        System.out.println(num1 + " - " + num2 + " = " + subtract);
+        System.out.println(num1 + " * " + num2 + " = " + multiply);
+        System.out.println(num1 + " / " + num2 + " = " + divide);
+        System.out.println(num1 + " % " + num2 + " = " + modulo);
+    }
+}
