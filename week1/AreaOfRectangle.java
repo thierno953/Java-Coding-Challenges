c634b4ff06e73acc35420325fedd3974e7a41e13
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-20 23:25:13 +0200

    AreaOfRectangle

---
+++ week1/AreaOfRectangle.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+
+        int length = 10;
+        int breadth = 30;
+
+        int areaOfRectangle = length * breadth;
+
+        System.out.println("Area of the pool is " + areaOfRectangle + " square units");
+    }
+}
