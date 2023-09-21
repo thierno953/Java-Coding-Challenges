f617f968a941bd1ecab619f69aa3ab664e87527c
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-21 10:12:59 +0200

    week1

--- week1/FizzBuzz.java
+++ week1/FizzBuzz.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in); 
+ 
         System.out.println("Please enter a number: ");
         int userInput = scanner.nextInt();
 
-        for (int i = 1; i <= userInput; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+        for (int i = 1; i <= userInput; i++) { 
+            if (i % 3 == 0 && i % 5 == 0) { 
                 System.out.println("FizzBuzz");
             } else if (i % 3 == 0) {
                 System.out.println("Fizz");
