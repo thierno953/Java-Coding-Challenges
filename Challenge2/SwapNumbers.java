a8d80a86c713fb8faa18fa0dd86e69192cabf3a1
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-21 12:03:55 +0200

    Challenge 2

---
+++ Challenge2/SwapNumbers.java
@@ -0,0 +1,15 @@
+package Challenge2;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+
+        // swapping values
+        int temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("X: " + x + " Y: " + y);
+    }
+}
