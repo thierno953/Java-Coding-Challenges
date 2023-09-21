67d95d0a99c1c01d895403252468c1b6c1af0864
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-21 09:13:34 +0200

    week1

---
+++ week1/DynamicCalculator.java
@@ -0,0 +1,40 @@
+package week1;
+
+import java.util.Scanner;
+
+public class DynamicCalculator {
+    public static void main(String[] args) {
+
+        final String SUM = "+";
+        final String SUBTRACT = "=";
+        final String MULTIPLY = "*";
+        final String DIVIDE = "/";
+
+        Scanner scanner = new Scanner(System.in);
+        double total = 0;
+
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Enter the operator(symbol) you would like to use x: ");
+        scanner.nextLine();
+        String operator = scanner.nextLine();
+
+        if (operator.equals(SUM)) {
+            total = num1 + num2;
+        } else if (operator.equals(SUBTRACT)) {
+            total = num1 - num2;
+        } else if (operator.equals(MULTIPLY)) {
+            total = num1 * num2;
+        } else if (operator.equals(DIVIDE)) {
+            total = num1 / num2;
+        } else {
+            System.out.println("Operation failed. Please type the right operator.");
+            System.exit(0);
+        }
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + total);
+
+        scanner.close();
+    }
+}
