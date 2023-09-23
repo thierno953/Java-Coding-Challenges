f7d9da12bdc7518867f2be347af685b76d458d12
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-23 11:37:52 +0200

    Challenge java

---
+++ Challenge1/Day2/TemperatureConversion.java
@@ -0,0 +1,10 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        double tempInCelsius = 24.0;
+
+        double tempInFahrenheit = (tempInCelsius * 9 / 5) + 32;
+
+        System.out.println(tempInCelsius + "C is equal to " + tempInFahrenheit + "F");
+    }
+}
+
