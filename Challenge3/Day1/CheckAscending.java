60cab4c75a72ab04668b49e5526832762189dbd9
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-09-29 11:18:09 +0200

    Challenge java

--- Challenge3/Day1/CheckAscending.java
+++ Challenge3/Day1/CheckAscending.java
@@ -2,7 +2,7 @@ package Challenge3.Day1;
 
 public class CheckAscending {
     public static void main(String[] args) {
-        int[] newArray = { 1, 3, 4, 3 };
+        int[] newArray = { 1, 3, 4, 3 }; 
 
         for (int i = 0; i < newArray.length; i++) {
             if (newArray[i] > newArray[i + 1]) {
